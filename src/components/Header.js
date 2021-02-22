38a29367b543e5737b22766e98062fa5fa89142f
Adam Kent <adamkentdev@gmail.com>
2021-02-22 14:26:09 -0700

    quick and dirty hook

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
 import {connect} from 'react-redux';
+import {getUser} from '../redux/userReducer';
 
-const Header = (props) => {
+class Header extends React.Component {
+
+    componentDidMount(){
+        this.props.getUser();
+    }
+
+    render(){
     return <div className="header">
-        {props.isLoggedIn ? 
+        {this.props.isLoggedIn ? 
         <div>
-            <h5>Welcome, {props.user.username} </h5>
+            <h5>Welcome, {this.props.user.username} </h5>
             <Link to="/">Login/Signup</Link>
             <Link to="/main">Main</Link>
         </div>
         :
         <h1>Please Login</h1>
         }
-        
     </div>
+    }
 }
 
 const mapStateToProps = state => state;
 
-export default connect(mapStateToProps)(Header);
+export default connect(mapStateToProps, {getUser})(Header);
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Main = () => {
+    const [imgUrl, setUrl] = useState('');
+    const [userInfo, setUserInfo] = useState({
+        email: '',
+        password: '',
+        username:''
+    })
+    // const [email, setEmail] = useState('');
+    // const [password, setPassword] = useState('');
+    // const [username, setUsername] = useState('');
+
+    // * axios.post('/auth/login', userInfo)
     return <div className="main">
         This is the Main component
+        <input
+            value={userInfo.username}
+            onChange={e => setUserInfo({...userInfo, username: e.target.value})}
+        />
+    
+        <h1>{userInfo.username}</h1>
     </div>
 }
 
